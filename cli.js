0122e4e21bfda37405b8985cf140e8c1ed6158ff
VS <valeriesho@unc.edu>
2022-11-30 16:43:05 -0500

    fix for package

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const minimist = require("minimist");
-const moment = require("moment-timezone");
-const fetch = require("node-fetch");
+import minimist from "minimist";
+import moment from "moment-timezone";
+import fetch from "node-fetch";
 
 var args = minimist(process.argv.slice(2));
 
